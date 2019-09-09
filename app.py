1327a0630a8e5a190d1feed74d43d1f7cec3a2a1
jezlax <cory.jez1@gmail.com>
2019-09-08 19:40:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ VALID_USERNAME_PASSWORD_PAIRS = {
     'callaway': 'epic'
 }
 
-server = flask.Flask(__name__)
+server = app.server
 app = dash.Dash(__name__, server=server, external_stylesheets=EXTERNAL_STYLESHEETS)
 auth = dash_auth.BasicAuth(
     app,
