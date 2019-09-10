c428159c365beedfa345c673e7cc0dd5281ddaf0
jezlax <cory.jez1@gmail.com>
2019-09-09 21:46:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output, State, Event
 import flask
 import dash_auth
+import os
+from random import randint
+import plotly.plotly as py
+from plotly.graph_objs import *
+
+
+
 
 # import pandas as pd
 EXTERNAL_STYLESHEETS = [
@@ -18,10 +26,28 @@ VALID_USERNAME_PASSWORD_PAIRS = {
     'callaway': 'epic'
 }
 
-app = dash.Dash(__name__, external_stylesheets=EXTERNAL_STYLESHEETS)
+
+# Setup the app
+# Make sure not to change this file name or the variable names below,
+# the template is configured to execute 'server' on 'app.py'
+
+server = flask.Flask(__name__)
+server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+
+app = dash.Dash(__name__, server=server external_stylesheets=EXTERNAL_STYLESHEETS)
 server = app.server
 auth = dash_auth.BasicAuth(
     app,
     VALID_USERNAME_PASSWORD_PAIRS
 )
 app.config.suppress_callback_exceptions = True
+
+
+
+
+# Put your Dash code here
+
+
+# Run the Dash app
+if __name__ == '__main__':
+    app.server.run(debug=True, threaded=True)
