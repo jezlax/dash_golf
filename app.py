0b93e993efd62ff70098380cca1a9e62ce02cfa7
jezlax <cory.jez1@gmail.com>
2019-09-09 21:52:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State, Event
+from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 import flask
 import dash_auth
